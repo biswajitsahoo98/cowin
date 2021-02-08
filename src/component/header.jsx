18949dee28b94b1a9134d360b28f5677b53f8320
biswajit <biswajitsahoo946@gmail.com>
2021-02-08 20:10:12 +0530

    logo name changed

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -14,7 +14,7 @@ export class Header extends Component {
         return (
             <div>
                 <header className="head">
-                    <a href="/"><img className="logo_1" src="img/cowin.png" alt="logo" width="150"></img></a>
+                    <a href="/"><img className="logo_1" src="img/cowin.png" alt="logomain" width="150"></img></a>
                     <img className="logo_3" src="img/national-emblem-login.svg" alt="logo" width="40"></img>
                     <div className="logo_2">
                     <Dropdown.Button overlay={menu} icon={<Avatar src={"https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"} />}>
